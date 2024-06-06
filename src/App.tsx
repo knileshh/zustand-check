1d1dee2cc37787fad74cd23cb828cac35e53f048
knilesh9 <neutron_stays@protonmail.com>
2024-06-06 10:07:02 +0530

    Zustand demo 1 not optimised

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import { Button } from "./components/ui/button"
+import { create } from 'zustand'
 
-function App() {
+const useStore = create<{
+  count:number,
+  inc: () => void;
+  dec: () => void;
+}>((set) => ({
+  count: 0,
+  inc: () => set((state) => ({count: state.count + 1})),
+  dec: () => set((state) => ({count: state.count - 1})),
+}))
+
+function App(){
+  const store = useStore();
+  
   return (
     <>
-    <Button> Click me! </Button>
+    <Button onClick={store.inc}>+</Button>
+    {/* {store.count} */}
+    <Count/>
+    <Button onClick={store.dec}>-</Button>
     </>
-  )
+  );
+}
+
+
+function Count(){
+  const store = useStore();
+  return <>{store.count}</>
 }
 
 export default App
